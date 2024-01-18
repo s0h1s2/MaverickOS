558b4464937b9a0647ef26bf1fc55d16818a9774
shkar <shkarhsardar@gmail.com>
2024-01-18 13:41:56 +0300

    stage2 loading

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-os: main.s
+os: main.s stage2
 	nasm main.s -f bin -o bootloader.bin
+	cat bootloader.bin stage2.bin > os.bin
+	rm bootloader.bin stage2.bin
+	dd if=os.bin of=disk.img bs=1024 count=1440
+	virtualboxvm --startvm Mavrick
+
+stage2: stage2.s
+	nasm stage2.s -f bin -o stage2.bin
+
--- main.s
+++ main.s
@@ -1,13 +1,42 @@
 [ORG 0x7c00]
+jmp start
+read_stage2_into_memory:
+    ;pusha 
+    ; Buffer to read.
+    mov dx,0 ; Number of retries.
+    mov ax,0
+    mov es,ax
+    mov bx,0x7e00
+    ;
+    mov ah,0x2
+    mov al,0x2
+    mov ch,0x0
+    mov cl,0x2
+    mov dh,0x0
+    .retry:
+    int 0x13
+    jc .error
+    jmp .end
+.error:
+    mov si,disk_error_msg
+    call print_str
+    cmp dx,0x3
+    jle .retry
+.end:
+    ;popa
+    ret
 start:
-    cld
+    xor ax,ax 
+    mov es,ax
+    mov ds,ax
     mov si,msg
     call print_str
-    mov dx,print_str
-    call print_hex
+    call read_stage2_into_memory
+    jmp 0x0000:0x7e00
     jmp $
     
-msg db 'Hello,Bootsector',0xa,0xd,0
+msg db 'Stage 1 loaded',0xa,0xd,0
+disk_error_msg db 'Unable to read disk',0xa,0xd,0
 %include "print.s"
 times 510-($-$$) db 0 ; 2 bytes less now
 db 0x55
